c6d9aa3cdc0e5166595965358c83dfa806533fa5
danbatiste <danbatiste@gmail.com>
2018-11-26 13:00:50 -0800

    Created initial filesystem

---
+++ src/board/Board.java
@@ -0,0 +1,6 @@
+package board;
+
+public class Board {
+    public static final int SIZE = 3;
+    
+}
---
+++ src/main/Interface.java
@@ -0,0 +1,37 @@
+package main;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import board.Board;
+
+public class Interface extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage ) {
+        primaryStage.setTitle("Chess");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+
+}
